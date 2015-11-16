74d0cb67013066add53cfbbdba4dc2d4b03a09a5
Aleen <aleen42@vip.qq.com>
2015-11-16 15:45:57 +0800

    update

--- monitor.sh
+++ monitor.sh
@@ -3,25 +3,25 @@
 # Allan.Chan
 DAT="`date +%Y%m%d`"
 HOUR="`date +%H`"
-DIR="/home/checkServer/oslog/host_${DAT}/${HOUR}"
+DIR="`pwd`/oslog/host_${DAT}/${HOUR}"
 DELAY=60
 COUNT=60
 # whether the responsible directory exist
 if ! test -d ${DIR}
 then
-        /bin/mkdir -p ${DIR}
+        `which mkdir` -p ${DIR}
 fi
 # general check
 export TERM=linux
-/usr/bin/top -b -d ${DELAY} -n ${COUNT} > ${DIR}/top_${DAT}.log 2>&1 &
+`which top` -b -d ${DELAY} -n ${COUNT} > ${DIR}/top_${DAT}.log 2>&1 &
 # cpu check
-/usr/bin/sar -u ${DELAY} ${COUNT} > ${DIR}/cpu_${DAT}.log 2>&1 &
+`which sar` -u ${DELAY} ${COUNT} > ${DIR}/cpu_${DAT}.log 2>&1 &
 #/usr/bin/mpstat -P 0 ${DELAY} ${COUNT} > ${DIR}/cpu_0_${DAT}.log 2>&1 &
 #/usr/bin/mpstat -P 1 ${DELAY} ${COUNT} > ${DIR}/cpu_1_${DAT}.log 2>&1 &
 # memory check
-/usr/bin/vmstat ${DELAY} ${COUNT} > ${DIR}/vmstat_${DAT}.log 2>&1 &
+`which vmstat` ${DELAY} ${COUNT} > ${DIR}/vmstat_${DAT}.log 2>&1 &
 # I/O check
-/usr/bin/iostat ${DELAY} ${COUNT} > ${DIR}/iostat_${DAT}.log 2>&1 &
+`which iostat` ${DELAY} ${COUNT} > ${DIR}/iostat_${DAT}.log 2>&1 &
 # network check
-/usr/bin/sar -n DEV ${DELAY} ${COUNT} > ${DIR}/net_${DAT}.log 2>&1 &
+`which sar` -n DEV ${DELAY} ${COUNT} > ${DIR}/net_${DAT}.log 2>&1 &
 #/usr/bin/sar -n EDEV ${DELAY} ${COUNT} > ${DIR}/net_edev_${DAT}.log 2>&1 &
