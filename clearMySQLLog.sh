f827e4ecd990f71413d973216945c3e1ddb3fff1
aleen42 <aleen42@vip.qq.com>
2016-07-20 09:43:13 +0800

    update

--- clearMySQLLog.sh
+++ clearMySQLLog.sh
@@ -3,7 +3,9 @@
 DeleteNum=`cat /var/log/mysql/mysql.log | grep delete | wc -l | awk '{print int($1)}'`
 DropNum=`cat /var/log/mysql/mysql.log | grep "DROP TABLE" | wc -l | awk '{print int($1)}'`
 
-if [ $DeleteNum -lt 1] || [ $DropNum -lt 1 ];then
+if [ $DeleteNum -lt 1] && [ $DropNum -lt 1 ];then
 	`echo "" > /var/log/mysql/mysql.log`;
+	`echo 0 > ~/checkServer/delete.log`;
+	`echo 0 > ~/checkServer/drop.log`;
 fi
 
